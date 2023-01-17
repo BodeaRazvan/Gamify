11b38c72df51b42999650f2706a843ccc18dc8c1
ituanca <itu_anca@yahoo.com>
2023-01-17 20:12:28 +0200

    small changes to streak page

--- gamify/src/mainPage/MainPage.js
+++ gamify/src/mainPage/MainPage.js
@@ -1,70 +1,18 @@
 import '../App.css';
 import '../index.css';
 import {Navbar} from "react-bootstrap";
-import {ProSidebar, Menu, MenuItem, SubMenu} from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
-import {useNavigate} from "react-router";
 import React, {useEffect} from "react";
 import GenerateAvatar from "../avatar/GenerateAvatar";
+import SidebarMainPage from "../sidebarAndNavbar/SidebarMainPage";
 
 
 function MainPage() {
 
-
-    let navigate = useNavigate();
-    const goToLogin = () => {
-        navigate('/login');
-    }
-    const goToRegister = () => {
-        navigate('/register');
-    }
-
-    const logOut = () => {
-        navigate('/');
-    }
-
     return(
         <div>
-            <Navbar sticky="top" className="navbar" style={{fontFamily: "poppins"}}>
-                <div>
-                    <p className="navBarTitle" style={{display: "inline", color: "white"}}>Gamify</p>
-                    <p className="navBarStreak" style={{display: "inline", color: "white"}}>Streak:
-                        <p style={{display: "inline", color: "yellow"}}>
-                        12 days
-                    </p></p>
-                    <p className="navBarNotification" style={{display: "inline", color: "white"}}>Notifications:
-                    <p style={{display: "inline", color: "red"}}>
-                         3
-                    </p>
-                    </p>
-                    <button className="navbarButton" onClick={logOut}> Log out</button>
-                </div>
-            </Navbar>
-            <ProSidebar style={{
-                marginTop: 48,
-                position: "fixed",
-                fontFamily: "poppins",
-                sticky: "top",
-                width: "150px",
-                minWidth: "150px",
-                fontSize: "20px",
-            }} className="mySideBar">
-                <Menu iconShape="square">
-                    <MenuItem onClick={() => {}}>Menu</MenuItem>
-                    <SubMenu title="Profile" onClick={() => navigate("/profile")}>
-                    </SubMenu>
-                    <SubMenu title="Games">
-                        <MenuItem onClick={() =>  {localStorage.setItem("gameOption","1");navigate("/chooseCourseSubject")}}> Classic quiz</MenuItem>
-                        <MenuItem onClick={() =>  {localStorage.setItem("gameOption","2");navigate("/chooseCourseSubject")}}> Approximate</MenuItem>
-                        <MenuItem onClick={() =>  {localStorage.setItem("gameOption","3");navigate("/chooseCourseSubject")}}> Columns</MenuItem>
-                        <MenuItem onClick={() => {localStorage.setItem("gameOption","4");navigate("/chooseCourseSubject")}}> Drag & Drop</MenuItem>
-                    </SubMenu>
-                    <SubMenu title="Courses" onClick={() => {localStorage.setItem("gameOption","5");navigate("/chooseCourseSubject")}}>
-                    </SubMenu>
-                    <SubMenu title="Contest" onClick={() => navigate("/contest")}>
-                    </SubMenu>
-                </Menu>
-            </ProSidebar>
+            <Navbar/>
+            <SidebarMainPage/>
             <div className="App" style={{fontFamily:"poppins"}}>
                 <header className="myHeader">
                    <GenerateAvatar/>
